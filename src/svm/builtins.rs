42111f08907cb2dd63ab81521b55c103b9dd5268
Phat Luu <yadsendew@protonmail.com>
2024-06-04 14:47:18 +0700

    add svm mod

---
+++ src/svm/builtins.rs
@@ -0,0 +1,2 @@
+pub const PARALLEL_HELLO_WORLD_CODE: &str =
+    include_str!("../builtins_code/parallel_hello_world.bend");
---
+++ src/svm/mod.rs
@@ -0,0 +1,56 @@
+use bend::{
+    diagnostics::{Diagnostics, DiagnosticsConfig},
+    fun::{self, load_book::do_parse_book, Book, Term},
+    run_book, CompileOpts, RunOpts,
+};
+use std::path::Path;
+
+pub mod builtins;
+
+pub fn run_code(
+    code: &str,
+    arguments: Option<Vec<Term>>,
+) -> Result<Option<(Term, String, Diagnostics)>, Diagnostics> {
+    let load_book = || -> Result<Book, Diagnostics> {
+        let builtins = fun::Book::builtins();
+        let book = do_parse_book(code, Path::new(""), builtins)?;
+        Ok(book)
+    };
+    let book = load_book().expect("lb failed");
+
+    let run_opts = RunOpts {
+        linear_readback: false,
+        pretty: false,
+        hvm_path: "hvm".to_owned(),
+    };
+
+    let compile_opts = CompileOpts {
+        eta: true,
+        prune: false,
+        linearize_matches: bend::OptLevel::Enabled,
+        float_combinators: true,
+        merge: false,
+        inline: false,
+        check_net_size: false,
+        adt_encoding: bend::AdtEncoding::NumScott,
+    };
+    let diagnostics_cfg = DiagnosticsConfig {
+        verbose: false,
+        irrefutable_match: bend::diagnostics::Severity::Allow,
+        redundant_match: bend::diagnostics::Severity::Allow,
+        unreachable_match: bend::diagnostics::Severity::Allow,
+        unused_definition: bend::diagnostics::Severity::Allow,
+        repeated_bind: bend::diagnostics::Severity::Allow,
+        recursion_cycle: bend::diagnostics::Severity::Allow,
+    };
+    let run_cmd = "run";
+
+    run_book(
+        book,
+        run_opts,
+        compile_opts,
+        diagnostics_cfg,
+        arguments,
+        run_cmd,
+    )
+}
