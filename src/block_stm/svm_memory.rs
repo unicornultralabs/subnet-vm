31c839be31bb590039560e4c3d81491e264b4a43
Phat Luu <yadsendew@protonmail.com>
2024-06-04 16:48:39 +0700

    fix err

--- src/block_stm/svm_memory.rs
+++ src/block_stm/svm_memory.rs
@@ -3,7 +3,7 @@ use crate::svm::{
     primitive_types::SVMPrimitives,
 };
 use dashmap::DashMap;
-use std::{collections::HashMap, sync::Arc};
+use std::{collections::HashMap, sync::Arc, thread::sleep, time::Duration};
 
 #[derive(Clone)]
 pub struct SVMMemory {
@@ -58,22 +58,51 @@ impl<'a> Transaction<'a> {
         self.write_set.insert(key, value);
     }
 
-    fn commit(self) -> bool {
+    fn commit(&self) -> Result<(), &'static str> {
         // Validate
-        for (key, (value, version)) in self.read_set {
-            if let Some(tv) = self.tm.objects.get(&key) {
-                if tv.version != version {
-                    return false; // Conflict detected, abort
+        for (key, (_, version)) in &self.read_set {
+            if let Some(tv) = self.tm.objects.get(key) {
+                if tv.version != *version {
+                    return Err("Conflict detected, transaction aborted");
                 }
             }
         }
 
         // Commit
-        for (key, value) in self.write_set {
-            let version = self.tm.objects.get(&key).map_or(0, |tv| tv.version) + 1;
-            self.tm.objects.insert(key, SVMObject { value, version });
+        for (key, value) in &self.write_set {
+            let version = self.tm.objects.get(key).map_or(0, |tv| tv.version) + 1;
+            self.tm.objects.insert(
+                key.clone(),
+                SVMObject {
+                    value: value.clone(),
+                    version,
+                },
+            );
         }
 
-        true
+        Ok(())
+    }
+
+    fn rollback(&mut self) {
+        self.read_set.clear();
+        self.write_set.clear();
+    }
+}
+
+fn retry_transaction<F>(tm: &SVMMemory, transaction_fn: F)
+where
+    F: Fn(&mut Transaction) -> (),
+{
+    loop {
+        let mut txn = Transaction::new(tm);
+        transaction_fn(&mut txn);
+
+        match txn.commit() {
+            Ok(_) => break,
+            Err(_) => {
+                txn.rollback();
+                sleep(Duration::from_millis(10)); // Simple backoff strategy
+            }
+        }
     }
 }
