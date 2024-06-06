07cd3fc876d45a4a49ebbc3db53310762607fd67
Phat Luu <yadsendew@protonmail.com>
2024-06-06 20:02:55 +0700

    you try to hard but it's so weird

--- src/svm/svm.rs
+++ src/svm/svm.rs
@@ -3,14 +3,17 @@ use bend::{
     compile_book,
     diagnostics::{Diagnostics, DiagnosticsConfig},
     fun::{self, load_book::do_parse_book, Book, Term},
+    hvm::display_hvm_tree,
     readback_hvm_net, CompileOpts, CompileResult, RunOpts,
 };
 use builtins::{ADD_CODE, ADD_CODE_ID, SUB_CODE, SUB_CODE_ID};
 use hvm::hvm::{GNet, TMem};
+use log::info;
 use std::{collections::HashMap, path::Path, sync::Arc, time::Instant};
 
 pub struct SVM {
-    books: Arc<HashMap<String, Book>>,
+    // code hash -> HVM code
+    books: Arc<HashMap<String, String>>,
 }
 
 impl SVM {
@@ -25,8 +28,53 @@ impl SVM {
         ];
         for code in codes {
             let builtins = builtins.clone();
-            let book = do_parse_book(code.1, Path::new(""), builtins).expect("lb failed");
-            books.insert(code.0.to_string(), book);
+
+            // let mut book = do_parse_book(code.1, Path::new(""), builtins).expect("lb failed");
+            // let compile_opts = CompileOpts {
+            //     eta: true,
+            //     prune: false,
+            //     linearize_matches: bend::OptLevel::Enabled,
+            //     float_combinators: true,
+            //     merge: false,
+            //     inline: false,
+            //     check_net_size: false,
+            //     adt_encoding: bend::AdtEncoding::NumScott,
+            // };
+            // let diagnostics_cfg = DiagnosticsConfig {
+            //     verbose: false,
+            //     irrefutable_match: bend::diagnostics::Severity::Allow,
+            //     redundant_match: bend::diagnostics::Severity::Allow,
+            //     unreachable_match: bend::diagnostics::Severity::Allow,
+            //     unused_definition: bend::diagnostics::Severity::Allow,
+            //     repeated_bind: bend::diagnostics::Severity::Allow,
+            //     recursion_cycle: bend::diagnostics::Severity::Allow,
+            // };
+            // let compile_res = compile_book(&mut book, compile_opts, diagnostics_cfg, None)
+            //     .expect("unable to compile");
+
+            // let mut hvm_book = String::from("");
+            // for (nam, def) in compile_res.hvm_book.defs.iter() {
+            //     hvm_book.push_str(&format!(
+            //         "{}\n",
+            //         format!("@{} = {}", nam, display_hvm_tree(&def.root))
+            //     ));
+            //     for (pri, a, b) in def.rbag.iter() {
+            //         hvm_book.push_str(&format!(
+            //             "{}\n",
+            //             format!(
+            //                 "  &{}{} ~ {}",
+            //                 if *pri { "!" } else { " " },
+            //                 display_hvm_tree(a),
+            //                 display_hvm_tree(b)
+            //             )
+            //         ));
+            //     }
+            //     hvm_book.push_str(&format!("\n"));
+            // }
+            // println!("-------");
+            // println!("{}", hvm_book);
+            let hvm_book = Self::gen_hvm_code(code.1, builtins.clone());
+            books.insert(code.0.to_string(), hvm_book);
         }
 
         Self {
@@ -34,15 +82,8 @@ impl SVM {
         }
     }
 
-    pub fn run_code(
-        self: Arc<Self>,
-        code_id: &str,
-        // TODO(rameight): HVM2 doesn't enable entrypoint running yet.
-        // entrypoint: Option<&str>,
-        arguments: Option<Vec<Term>>,
-    ) -> Result<Option<(Term, String, Diagnostics)>, Diagnostics> {
-        let book = self.books.get(code_id).expect("load book failed").clone();
-
+    fn gen_hvm_code(bend_code: &str, builtins: bend::fun::Book) -> String {
+        let mut book = do_parse_book(&bend_code, Path::new(""), builtins).expect("lb failed");
         let compile_opts = CompileOpts {
             eta: true,
             prune: false,
@@ -62,137 +103,221 @@ impl SVM {
             repeated_bind: bend::diagnostics::Severity::Allow,
             recursion_cycle: bend::diagnostics::Severity::Allow,
         };
-        self.run_book(book, compile_opts, diagnostics_cfg, arguments)
+        let compile_res = compile_book(&mut book, compile_opts, diagnostics_cfg, None)
+            .expect("unable to compile");
 
-        // TODO(rameight): by calling the hvm binary, it does not work as expected
-        // since it fails to streamlining the VM result
-        // run_book(
-        //     book,
-        //     run_opts,
-        //     compile_opts,
-        //     diagnostics_cfg,
-        //     arguments,
-        //     "run",
-        // )
+        let mut hvm_book = String::from("");
+        for (nam, def) in compile_res.hvm_book.defs.iter() {
+            hvm_book.push_str(&format!(
+                "{}\n",
+                format!("@{} = {}", nam, display_hvm_tree(&def.root))
+            ));
+            for (pri, a, b) in def.rbag.iter() {
+                hvm_book.push_str(&format!(
+                    "{}\n",
+                    format!(
+                        "  &{}{} ~ {}",
+                        if *pri { "!" } else { " " },
+                        display_hvm_tree(a),
+                        display_hvm_tree(b)
+                    )
+                ));
+            }
+            hvm_book.push_str(&format!("\n"));
+        }
+        println!("-------");
+        println!("{}", hvm_book);
+        hvm_book
     }
 
-    pub fn run_code_with_timers(
+    // pub fn run_code(
+    //     self: Arc<Self>,
+    //     code_id: &str,
+    //     // TODO(rameight): HVM2 doesn't enable entrypoint running yet.
+    //     // entrypoint: Option<&str>,
+    //     arguments: Option<Vec<Term>>,
+    // ) -> Result<Option<(Term, String, Diagnostics)>, Diagnostics> {
+    //     let book = self.books.get(code_id).expect("load book failed").clone();
+
+    //     let compile_opts = CompileOpts {
+    //         eta: true,
+    //         prune: false,
+    //         linearize_matches: bend::OptLevel::Enabled,
+    //         float_combinators: true,
+    //         merge: false,
+    //         inline: false,
+    //         check_net_size: false,
+    //         adt_encoding: bend::AdtEncoding::NumScott,
+    //     };
+    //     let diagnostics_cfg = DiagnosticsConfig {
+    //         verbose: false,
+    //         irrefutable_match: bend::diagnostics::Severity::Allow,
+    //         redundant_match: bend::diagnostics::Severity::Allow,
+    //         unreachable_match: bend::diagnostics::Severity::Allow,
+    //         unused_definition: bend::diagnostics::Severity::Allow,
+    //         repeated_bind: bend::diagnostics::Severity::Allow,
+    //         recursion_cycle: bend::diagnostics::Severity::Allow,
+    //     };
+    //     self.run_book(book, compile_opts, diagnostics_cfg, arguments)
+
+    //     // TODO(rameight): by calling the hvm binary, it does not work as expected
+    //     // since it fails to streamlining the VM result
+    //     // run_book(
+    //     //     book,
+    //     //     run_opts,
+    //     //     compile_opts,
+    //     //     diagnostics_cfg,
+    //     //     arguments,
+    //     //     "run",
+    //     // )
+    // }
+
+    pub fn run_hvm_code(
         self: Arc<Self>,
         code_id: &str,
-        // TODO(rameight): HVM2 doesn't enable entrypoint running yet.
-        // entrypoint: Option<&str>,
         arguments: Option<Vec<Term>>,
-    ) -> (
-        Result<Option<(Term, String, Diagnostics)>, Diagnostics>,
-        (u128, u128, u128),
-    ) {
-        let book = self.books.get(code_id).expect("load book failed").clone();
-
-        let compile_opts = CompileOpts {
-            eta: true,
-            prune: false,
-            linearize_matches: bend::OptLevel::Enabled,
-            float_combinators: true,
-            merge: false,
-            inline: false,
-            check_net_size: false,
-            adt_encoding: bend::AdtEncoding::NumScott,
-        };
-        let diagnostics_cfg = DiagnosticsConfig {
-            verbose: false,
-            irrefutable_match: bend::diagnostics::Severity::Allow,
-            redundant_match: bend::diagnostics::Severity::Allow,
-            unreachable_match: bend::diagnostics::Severity::Allow,
-            unused_definition: bend::diagnostics::Severity::Allow,
-            repeated_bind: bend::diagnostics::Severity::Allow,
-            recursion_cycle: bend::diagnostics::Severity::Allow,
-        };
-        self.run_book_with_timers(book, compile_opts, diagnostics_cfg, arguments)
-
-        // TODO(rameight): by calling the hvm binary, it does not work as expected
-        // since it fails to streamlining the VM result
-        // run_book(
-        //     book,
-        //     run_opts,
-        //     compile_opts,
-        //     diagnostics_cfg,
-        //     arguments,
-        //     "run",
-        // )
-    }
-
-    fn run_book_with_timers(
-        self: Arc<Self>,
-        mut book: Book,
-        compile_opts: CompileOpts,
-        diagnostics_cfg: DiagnosticsConfig,
-        args: Option<Vec<Term>>,
-    ) -> (
-        Result<Option<(Term, String, Diagnostics)>, Diagnostics>,
-        (u128, u128, u128),
-    ) {
-        let (mut compilation_mrs, mut run_hvm_mrs, mut readback_mrs) = (0, 0, 0);
-
-        let now = Instant::now();
-        let CompileResult {
-            hvm_book: core_book,
-            labels,
-            diagnostics,
-        } = match compile_book(&mut book, compile_opts.clone(), diagnostics_cfg, args) {
-            Ok(r) => r,
-            Err(e) => return (Err(e), (0, 0, 0)),
-        };
-        eprint!("{diagnostics}");
-        compilation_mrs += now.elapsed().as_micros();
-
-        let now = Instant::now();
-        let (net, stats) = match Self::run_hvm(&core_book.build()) {
-            Ok(r) => r,
-            Err(e) => return (Err(e.into()), (0, 0, 0)),
-        };
-        run_hvm_mrs += now.elapsed().as_micros();
-
-        let now = Instant::now();
-        let (term, diags) = readback_hvm_net(
-            &net,
-            &book,
-            &labels,
-            false, // linear_readback
-            compile_opts.adt_encoding,
-        );
-        readback_mrs += now.elapsed().as_micros();
-
-        (
-            Ok(Some((term, stats, diags))),
-            (compilation_mrs, run_hvm_mrs, readback_mrs),
-        )
-    }
-
-    fn run_book(
-        self: Arc<Self>,
-        mut book: Book,
-        compile_opts: CompileOpts,
-        diagnostics_cfg: DiagnosticsConfig,
-        args: Option<Vec<Term>>,
     ) -> Result<Option<(Term, String, Diagnostics)>, Diagnostics> {
-        let CompileResult {
-            hvm_book: core_book,
-            labels,
-            diagnostics,
-        } = compile_book(&mut book, compile_opts.clone(), diagnostics_cfg, args)?;
-        eprint!("{diagnostics}");
+        let code = self.books.get(code_id).expect("load book failed").clone();
+        let book = hvm::ast::Book::parse(&code)
+            .unwrap_or_else(|er| panic!("{}", er))
+            .build();
 
-        let (net, stats) = Self::run_hvm(&core_book.build())?;
-        let (term, diags) = readback_hvm_net(
-            &net,
-            &book,
-            &labels,
-            true, // linear_readback,
-            compile_opts.adt_encoding,
-        );
+        // TODO(rameight): replace arguments for prebuilt hvm code
 
-        Ok(Some((term, stats, diags)))
+        let book = hvm::ast::Book::parse(&code)
+            .unwrap_or_else(|er| panic!("{}", er))
+            .build();
+        let (net, stats) = Self::run_hvm(&book)?;
+        info!("{:#?}{:#?}", net, stats);
+        // let (term, diags) = readback_hvm_net(
+        //     &net,
+        //     &book,
+        //     &labels,
+        //     true, // linear_readback,
+        //     compile_opts.adt_encoding,
+        // );
+
+        // Ok(Some((term, stats, diags)));
+        Ok(None)
     }
+
+    // pub fn run_code_with_timers(
+    //     self: Arc<Self>,
+    //     code_id: &str,
+    //     // TODO(rameight): HVM2 doesn't enable entrypoint running yet.
+    //     // entrypoint: Option<&str>,
+    //     arguments: Option<Vec<Term>>,
+    // ) -> (
+    //     Result<Option<(Term, String, Diagnostics)>, Diagnostics>,
+    //     (u128, u128, u128),
+    // ) {
+    //     let book = self.books.get(code_id).expect("load book failed").clone();
+
+    //     let compile_opts = CompileOpts {
+    //         eta: true,
+    //         prune: false,
+    //         linearize_matches: bend::OptLevel::Enabled,
+    //         float_combinators: true,
+    //         merge: false,
+    //         inline: false,
+    //         check_net_size: false,
+    //         adt_encoding: bend::AdtEncoding::NumScott,
+    //     };
+    //     let diagnostics_cfg = DiagnosticsConfig {
+    //         verbose: false,
+    //         irrefutable_match: bend::diagnostics::Severity::Allow,
+    //         redundant_match: bend::diagnostics::Severity::Allow,
+    //         unreachable_match: bend::diagnostics::Severity::Allow,
+    //         unused_definition: bend::diagnostics::Severity::Allow,
+    //         repeated_bind: bend::diagnostics::Severity::Allow,
+    //         recursion_cycle: bend::diagnostics::Severity::Allow,
+    //     };
+    //     self.run_book_with_timers(book, compile_opts, diagnostics_cfg, arguments)
+
+    //     // TODO(rameight): by calling the hvm binary, it does not work as expected
+    //     // since it fails to streamlining the VM result
+    //     // run_book(
+    //     //     book,
+    //     //     run_opts,
+    //     //     compile_opts,
+    //     //     diagnostics_cfg,
+    //     //     arguments,
+    //     //     "run",
+    //     // )
+    // }
+
+    // fn run_book_with_timers(
+    //     self: Arc<Self>,
+    //     mut book: Book,
+    //     compile_opts: CompileOpts,
+    //     diagnostics_cfg: DiagnosticsConfig,
+    //     args: Option<Vec<Term>>,
+    // ) -> (
+    //     Result<Option<(Term, String, Diagnostics)>, Diagnostics>,
+    //     (u128, u128, u128),
+    // ) {
+    //     let (mut compilation_mrs, mut run_hvm_mrs, mut readback_mrs) = (0, 0, 0);
+
+    //     let now = Instant::now();
+    //     let CompileResult {
+    //         hvm_book: core_book,
+    //         labels,
+    //         diagnostics,
+    //     } = match compile_book(&mut book, compile_opts.clone(), diagnostics_cfg, args) {
+    //         Ok(r) => r,
+    //         Err(e) => return (Err(e), (0, 0, 0)),
+    //     };
+    //     eprint!("{diagnostics}");
+    //     compilation_mrs += now.elapsed().as_micros();
+
+    //     let now = Instant::now();
+    //     let (net, stats) = match Self::run_hvm(&core_book.build()) {
+    //         Ok(r) => r,
+    //         Err(e) => return (Err(e.into()), (0, 0, 0)),
+    //     };
+    //     run_hvm_mrs += now.elapsed().as_micros();
+
+    //     let now = Instant::now();
+    //     let (term, diags) = readback_hvm_net(
+    //         &net,
+    //         &book,
+    //         &labels,
+    //         false, // linear_readback
+    //         compile_opts.adt_encoding,
+    //     );
+    //     readback_mrs += now.elapsed().as_micros();
+
+    //     (
+    //         Ok(Some((term, stats, diags))),
+    //         (compilation_mrs, run_hvm_mrs, readback_mrs),
+    //     )
+    // }
+
+    // fn run_book(
+    //     self: Arc<Self>,
+    //     mut book: Book,
+    //     compile_opts: CompileOpts,
+    //     diagnostics_cfg: DiagnosticsConfig,
+    //     args: Option<Vec<Term>>,
+    // ) -> Result<Option<(Term, String, Diagnostics)>, Diagnostics> {
+    //     let CompileResult {
+    //         hvm_book: core_book,
+    //         labels,
+    //         diagnostics,
+    //     } = compile_book(&mut book, compile_opts.clone(), diagnostics_cfg, args)?;
+    //     eprint!("{diagnostics}");
+
+    //     let (net, stats) = Self::run_hvm(&core_book.build())?;
+    //     let (term, diags) = readback_hvm_net(
+    //         &net,
+    //         &book,
+    //         &labels,
+    //         true, // linear_readback,
+    //         compile_opts.adt_encoding,
+    //     );
+
+    //     Ok(Some((term, stats, diags)))
+    // }
 
     // fn run_book_c(
     //     self: Arc<Self>,
