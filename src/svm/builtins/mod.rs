1a758a02db7f5c0e7b00a5371bf7a1cfd0449409
Phat Luu <yadsendew@protonmail.com>
2024-06-07 15:59:45 +0700

    remove builtins

---
+++ src/svm/builtins/mod.rs
@@ -0,0 +1,13 @@
+pub const PARALLEL_HELLO_WORLD_CODE: &str = include_str!("./parallel_hello_world.bend");
+
+pub const ADD_CODE_ID: &str = "add";
+pub const ADD_CODE: &str = include_str!("./add.bend");
+
+pub const SUB_CODE_ID: &str = "sub";
+pub const SUB_CODE: &str = include_str!("./sub.bend");
+
+pub const TRANSFER_CODE_ID: &str = "0xtransfer";
+pub const TRANSFER_CODE: &str = include_str!("./transfer.bend");
+
+pub const DUANGUA_CODE_ID: &str = "0xduangua";
+pub const DUANGUA_CODE: &str = include_str!("./duangua.bend");
